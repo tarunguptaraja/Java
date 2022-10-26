3c1934b6b814feed1cfd4056a6e92c4304da7f9d
tanmayguptayash <68992803+tanmayguptayash@users.noreply.github.com>
2022-10-26 18:12:21 +0530

    Update BinaryToDecimal.java

--- src/main/java/com/thealgorithms/conversions/BinaryToDecimal.java
+++ src/main/java/com/thealgorithms/conversions/BinaryToDecimal.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 /**
  * This class converts a Binary number to a Decimal number
  */
+
 class BinaryToDecimal {
 
     /**
      * Main Method
-     *
+     * 
      * @param args Command line arguments
      */
+    
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int binNum, binCopy, d, s = 0, power = 0;
